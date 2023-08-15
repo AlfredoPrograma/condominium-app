f9696464f32dcdbafd2096665307fc1865e40f29
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2023-08-15 00:55:01 -0400

    feature: base sign in form styles

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,7 @@
-import { useSession, signIn } from "next-auth/react"
+import { signIn } from "next-auth/react"
+import Head from "next/head"
 import { ChangeEvent, FormEvent, useState } from "react"
+import { PageContainer } from "~/components/common/PageContainer"
 
 export default function SignIn() {
     const [signInForm, setSignInForm] = useState({
@@ -10,14 +12,14 @@ export default function SignIn() {
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         try {
             e.preventDefault()
-            const res = await signIn("credentials", { 
+            const res = await signIn("credentials", {
                 callbackUrl: "/",
                 email: signInForm.email,
                 password: signInForm.password
             })
 
             console.log(res)
-        } catch(err) {
+        } catch (err) {
             console.error(err)
         }
     }
@@ -25,15 +27,47 @@ export default function SignIn() {
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target
 
-        setSignInForm({...signInForm, [name]: value })
+        setSignInForm({ ...signInForm, [name]: value })
     }
 
     return (
-        <form onSubmit={(e) => handleSubmit(e)} className="card bg-white max-w-xs p-12 flex flex-col gap-4">
-            <input onChange={handleChange} value={signInForm.email} type="text" name="email"  className="input input-bordered w-full max-w-xs" />
-            <input onChange={handleChange} value={signInForm.password} type="password" name="password"  className="input input-bordered w-full max-w-xs" />
+        <PageContainer title="Sign in">
+            <section className="min-h-screen grid md:grid-cols-[1fr_2fr]">
+                <section className="grid place-items-center">
+                    <div className="flex flex-col items-center gap-6">
+                        <header className="flex flex-col gap-2 text-center">
+                            <h1 className="font-bold text-3xl">
+                                <span>¡Bienvenido a </span>
+                                <span className="text-primary">Condominium</span>
+                                <span>!</span>
+                            </h1>
 
-            <button type="submit" className="btn-primary rounded-md py-2">Sign in</button>
-        </form>
+                            <h2>Gestiona tus responsabilidades residenciales</h2>
+                        </header>
+                        <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col gap-4 max-w-xs">
+                            <div className="form-control w-full max-w-xs">
+                                <label htmlFor="email" className="label">
+                                    <span className="label-text font-semibold">Correo electrónico</span>
+                                </label>
+                                <input onChange={handleChange} value={signInForm.email} type="text" name="email" id="email" className="input input-bordered w-full max-w-xs" placeholder="usuario@mail.com" />
+                            </div>
+
+                            <div className="form-control w-full max-w xs">
+                                <label htmlFor="password" className="label">
+                                    <span className="label-text font-semibold">Contraseña</span>
+                                </label>
+                                <input onChange={handleChange} value={signInForm.password} type="password" name="password" id="password" className="input input-bordered w-full max-w-xs" placeholder="******" />
+                            </div>
+
+                            <button type="submit" className="btn-primary rounded-md py-2">Iniciar sesión</button>
+                        </form>
+                    </div>
+                </section>
+
+                <div className="bg-black hidden md:block">
+
+                </div>
+            </section>
+        </PageContainer>
     )
 }
