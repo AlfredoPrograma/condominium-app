d9f89a44c490892561a5e8cddc6403b0bb0d2f09
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2023-08-20 10:20:50 -0400

    chores: prepare environment variables mailing service

--- src/env.mjs
+++ src/env.mjs
@@ -20,6 +20,8 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string().min(1) : z.string().url()
     ),
+    NODEMAILER_PASSWORD: z.string(),
+    NODEMAILER_EMAIL: z.string().email()
   },
 
   /**
@@ -40,6 +42,8 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NODEMAILER_EMAIL: process.env.NODEMAILER_EMAIL,
+    NODEMAILER_PASSWORD: process.env.NODEMAILER_PASSWORD,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
